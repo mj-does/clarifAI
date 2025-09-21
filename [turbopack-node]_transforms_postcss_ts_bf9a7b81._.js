f5279a96ab2066caac99d0767af3e9b7b9cc9d45
DishaWalyat <dishawalyat@gmail.com>
2025-09-21 22:51:15 +0530

    Add files via upload

---
+++ [turbopack-node]_transforms_postcss_ts_bf9a7b81._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/career-dashboard/postcss.config.js_.loader.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "build/chunks/ad602_257c6bdc._.js",
+  "build/chunks/[root-of-the-server]__f7086713._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/career-dashboard/postcss.config.js_.loader.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SPjh5kWaisJ0KvmUswQMdEM22HHEU6hlvOumgAq0OUs=\"\n}"
